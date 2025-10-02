cc70ae59015d7927bfb10890878b33d1c701d905
k58701 <k58701@naver.com>
2025-10-02 20:45:49 +0900

    [TaeHeeLee0] 10월 2일 문제풀이
    [BOJ] 좌표 정렬하기 2 / Silver5 / 35분

---
+++ TaeHeeLee/10월/src/Week01/BOJ_11651_좌표정렬하기2.java
@@ -0,0 +1,38 @@
+package Week01;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_11651_좌표정렬하기2 {
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int N = Integer.parseInt(br.readLine());
+		int[][] arr = new int[N][2];
+		
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			arr[i][0] = Integer.parseInt(st.nextToken());  // x좌표
+			arr[i][1] = Integer.parseInt(st.nextToken());  // y좌표
+		}
+		
+		// 비교
+        Arrays.sort(arr, (a, b) -> {
+        	// y가 다르면 y좌표 비교
+            if (a[1] != b[1]) return Integer.compare(a[1], b[1]);
+            // y가 같다면 x좌표 비교
+            return Integer.compare(a[0], b[0]);
+        });
+
+        // 출력
+        for (int i = 0; i < N; i++) {
+            sb.append(arr[i][0]).append(' ').append(arr[i][1]).append('\n');
+        }
+        
+        System.out.print(sb);
+	}
+}
