de9b3aa76a4f096ee6d32d658fa3281d76e8785f
Son-juyeong <102614018+Son-juyeong@users.noreply.github.com>
2025-10-16 13:56:01 +0900

    [BOJ] 단지번호붙이기 / Silver1 / 30분

--- 10월/3주차/BOJ_2667_단지번호붙이기/Sonjuyeong.java
+++ 10월/3주차/BOJ_2667_단지번호붙이기/Sonjuyeong.java
@@ -1,5 +1,3 @@
-package samsung01;
-
 import java.io.*;
 import java.util.*;
 
@@ -26,7 +24,7 @@ public class Sonjuyeong {
 		n = Integer.parseInt(br.readLine());
 		char[][] arr = new char[n][n];
 		cnts = new ArrayList<>();
-		
+
 		for (int i = 0; i < n; i++) {
 			String str = br.readLine();
 			for (int j = 0; j < n; j++) {
@@ -43,35 +41,35 @@ public class Sonjuyeong {
 		}
 
 		Collections.sort(cnts);
-		
+
 		sb.append(cnts.size()).append("\n");
 		for (int i = 0; i < cnts.size(); i++) {
 			sb.append(cnts.get(i)).append("\n");
 		}
-		
+
 		System.out.println(sb);
 	}
 
 	public static void bfs(int r, int c, char[][] arr) {
 		Queue<Node> q = new ArrayDeque<>();
 		q.offer(new Node(r, c));
-		arr[r][c]='0';
-		
+		arr[r][c] = '0';
+
 		int cnt = 1;
-		
-		while(!q.isEmpty()) {
+
+		while (!q.isEmpty()) {
 			Node cur = q.poll();
-			for(int i = 0;i<4;i++) {
-				int nextR = cur.r+dr[i];
-				int nextC = cur.c+dc[i];
-				if(nextR>=0&&nextR<n&&nextC>=0&&nextC<n&&arr[nextR][nextC]=='1') {
-					arr[nextR][nextC]='0';
+			for (int i = 0; i < 4; i++) {
+				int nextR = cur.r + dr[i];
+				int nextC = cur.c + dc[i];
+				if (nextR >= 0 && nextR < n && nextC >= 0 && nextC < n && arr[nextR][nextC] == '1') {
+					arr[nextR][nextC] = '0';
 					++cnt;
 					q.offer(new Node(nextR, nextC));
 				}
 			}
 		}
-		
+
 		cnts.add(cnt);
 	}
 }
