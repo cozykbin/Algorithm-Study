f94150e015efa0a3fd8c27fde1ad4d7cbb54090e
Son-juyeong <102614018+Son-juyeong@users.noreply.github.com>
2025-10-21 11:27:27 +0900

    [BOJ] 다이나믹이 뭐에요? / Silver2 / 20분

--- 10월/4주차/BOJ_14494_다이나믹이 뭐에요/Sonjuyeong.java
+++ 10월/4주차/BOJ_14494_다이나믹이 뭐에요/Sonjuyeong.java
@@ -10,17 +10,19 @@ public class Sonjuyeong {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
-		long[][] dp = new long[n+1][m+1];
-		
-		dp[1][1]=1;
-		
-		for(int i = 1;i<=n;i++) {
-			for(int j = 1;j<=m;j++) {
-				if(i==1&&j==1) continue;
-				dp[i][j]+=(dp[i-1][j]+dp[i][j-1]+dp[i-1][j-1])%1000000007;
+		long[][] dp = new long[n + 1][m + 1];
+
+		dp[1][1] = 1;
+
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= m; j++) {
+				if (i == 1 && j == 1)
+					continue;
+				dp[i][j] += (dp[i - 1][j] + dp[i][j - 1] + dp[i - 1][j - 1]) % 1000000007;
 			}
 		}
-		
-		System.out.println(dp[n][m]%1000000007);
+
+		System.out.println(dp[n][m] % 1000000007);
 	}
+
 }
